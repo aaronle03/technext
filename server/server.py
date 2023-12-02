01fd8343c1232d9c159875b34566986af8c018e6
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2023-12-01 16:22:29 -0800

    server working

--- server/server.py
+++ server/server.py
@@ -1,55 +1,40 @@
-import os
-from pymongo import MongoClient
-from flask import Flask, request, jsonify
+from flask import Flask, jsonify
 from flask_cors import CORS
+from pymongo import MongoClient
+from pymongo.server_api import ServerApi
 
 app = Flask(__name__)
 CORS(app)
 
 
-def get_database():
-    connection_string = "mongodb+srv://aaronle5621:H372yQatE1xYIFjE@nextdata.5z7ay5h.mongodb.net/?retryWrites=true&w=majority"
-    if not connection_string:
-        raise ValueError(
-            "MongoDB connection string not found in environment variables."
-        )
+def convert_document(document):
+    # Convert ObjectId to string in the document
+    document["_id"] = str(document["_id"])
+    return document
 
-    try:
-        client = MongoClient(connection_string)
-        print("Connection to MongoDB successful!")
-        return client["nextdata"]
-    except Exception as e:
-        print(f"Error connecting to MongoDB: {e}")
-        raise
+
+def get_database():
+    uri = "mongodb+srv://aaronle5621:H372yQatE1xYIFjE@nextdata.5z7ay5h.mongodb.net/TechNext?retryWrites=true&w=majority"
+    client = MongoClient(uri, server_api=ServerApi("1"))
+    print("Connection to MongoDB successful!")
+    return client["TechNext"]
 
 
 def get_documents():
-    try:
-        dbname = get_database()
-        collection = dbname["technext"]
-        results = collection.find({})
-        return results
-    except Exception as e:
-        print(f"Error retrieving documents from MongoDB: {e}")
-        raise
+    dbname = get_database()
+    collection = dbname["nextdata"]
+    results = collection.find({})
+    return results
 
 
 @app.route("/api/documents", methods=["GET"])
 def document_api():
     try:
         documents = get_documents()
-        documentList = []
-        for doc in documents:
-            doc_info = {
-                "id": doc["idx"],
-                "id2": doc["idx_2"],
-                "patent_id": doc["patent_id"],
-                "patent_text": doc["patent_text"],
-                "phase": doc["phase"],
-                "date": doc["date"],
-            }
-            documentList.append(doc_info)
-        return jsonify(documentList)
+        # Convert each document's ObjectId to string
+        document_list = [convert_document(doc) for doc in documents]
+        print(document_list[0])  # Test print the first item
+        return jsonify(document_list)
     except Exception as e:
         print(f"Error processing documents: {e}")
         return jsonify({"error": "Internal Server Error"}), 500

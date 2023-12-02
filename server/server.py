1868614e9de01c893373a8f0387e16abdb6bd85e
Aaron Le <91503688+aaronle03@users.noreply.github.com>
2023-12-02 14:15:16 -0800

    search working, need to fix loading

--- server/server.py
+++ server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 from pymongo import MongoClient
 from pymongo.server_api import ServerApi
@@ -30,13 +30,43 @@ def get_documents():
 @app.route("/api/documents", methods=["GET"])
 def document_api():
     try:
-        documents = get_documents()
-        # Convert each document's ObjectId to string
+        page = request.args.get("page", default=1, type=int)
+        per_page = request.args.get("per_page", default=10, type=int)
+        skip = (page - 1) * per_page
+        documents = get_documents().skip(skip).limit(per_page)
         document_list = [convert_document(doc) for doc in documents]
-        print(document_list[0])  # Test print the first item
         return jsonify(document_list)
     except Exception as e:
         print(f"Error processing documents: {e}")
+        return jsonify({"error": "Internal Server Error"}), 500
+
+
+@app.route("/api/search", methods=["GET"])
+def search_api():
+    try:
+        query = request.args.get("query")
+
+        dbname = get_database()
+        collection = dbname["nextdata"]
+
+        # Use a regex pattern for case-insensitive search on multiple fields
+        results = collection.find(
+            {
+                "$or": [
+                    {"_id": {"$regex": query, "$options": "i"}},
+                    {"Date": {"$regex": query, "$options": "i"}},
+                    {"idx": {"$regex": query, "$options": "i"}},
+                    {"patent_id": {"$regex": query, "$options": "i"}},
+                    {"patent_text": {"$regex": query, "$options": "i"}},
+                    {"phase": {"$regex": query, "$options": "i"}},
+                ]
+            }
+        )
+
+        search_results = [convert_document(doc) for doc in results]
+        return jsonify(search_results)
+    except Exception as e:
+        print(f"Error processing search: {e}")
         return jsonify({"error": "Internal Server Error"}), 500
 
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,82 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import { debounce } from 'lodash';
 import './Home.css';
 
 const Home = () => {
+    const [items, setItems] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const [noResults, setNoResults] = useState(false);
+    const [hasMore, setHasMore] = useState(true);
+
+    const fetchDocs = async () => {
+        try {
+            const response = await axios.get('http://localhost:5000/api/documents');
+            setItems(response.data);
+            if (response.data.length < 10) {
+                setHasMore(false);
+            }
+        } catch (error) {
+            console.error('Error fetching data: ', error);
+        }
+    };
+
+    const handleSearch = debounce(async () => {
+        try {
+            console.log(`Sending search request with query: ${searchTerm}`);
+            const response = await axios.get(`http://localhost:5000/api/search?query=${searchTerm}`);
+            console.log('Search response:', response.data);
+            setItems(response.data);
+            setNoResults(response.data.length === 0);
+        } catch (error) {
+            console.error('Error searching data: ', error);
+        }
+    }, 500);
+    
+
+    useEffect(() => {
+        fetchDocs();
+    }, []);
+
     return (
         <div className="home">
             <div className="filters">
                 <h1>Search by Keyword, ID, or Publishing Date:</h1>
-                <input type="search" id="site-search" name="q" />
-                <button>Search</button>
+                <input
+                    type="search"
+                    id="site-search"
+                    name="q"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button onClick={handleSearch}>Search</button>
             </div>
+            <InfiniteScroll
+                dataLength={items.length}
+                next={handleSearch}
+                hasMore={hasMore}
+            ></InfiniteScroll>
             <div className="main-section">
-                <h1>Main Section</h1>
+                {noResults ? (
+                    <p>No results found.</p>
+                ) : (
+                    <ul className="item-list">
+                        {items.map((item) => (
+                            <li key={item._id}>
+                                <h2>Item ID: {item._id}</h2>
+                                <p>Date: {item.Date}</p>
+                                <p>Index: {item.idx}</p>
+                                <p>Patent ID: {item.patent_id}</p>
+                                <p>Patent Text: {item.patent_text}</p>
+                                <p>Phase: {item.phase}</p>
+                            </li>
+                        ))}
+                    </ul>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Home
+export default Home;
